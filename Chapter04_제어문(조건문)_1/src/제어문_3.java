f191af43f1595efa16c99f26a0cc547463982503
sist <sist@sist24>
2025-07-28 11:13:27 +0900

    제어문_1

---
+++ Chapter04_제어문(조건문)_1/src/제어문_3.java
@@ -0,0 +1,29 @@
+// 문자 1개를 난수 발생 => 대문자 / 소문자 
+
+public class 제어문_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int change=(int)(Math.random()*2);
+		//0.0~0.99 =? 0.0~1.999...
+		//=> 정수로 변경 => 0,1
+	    char c=' ';
+	    if(change==0) {
+	    	c=(char)((Math.random()*26)+65);
+	    	// 0~25 +65 => 0+65 =>'A' 
+	    	
+	    } else {
+	    	c=(char)((Math.random()*26)+97);
+	    	System.out.println("=====결과=====");
+	    }
+	     
+	    if(c>='A' && c<='Z') {
+	    	System.out.println(c + "는(은) 대문자입니다");
+		} else {
+			System.out.println(c + "는(은) 소문자입니다");
+		}
+	}
+}
+
+
+
