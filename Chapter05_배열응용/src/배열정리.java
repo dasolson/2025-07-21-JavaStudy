f4420e267847db73120dc309cd10a37c4721a10b
sist <sist@sist24>
2025-08-05 17:52:03 +0900

    배열응용

---
+++ Chapter05_배열응용/src/배열정리.java
@@ -0,0 +1,82 @@
+/*
+ *     1. 배열
+ *        1) 배열 선언
+ *            1.1 배열 갯수
+ *            
+ *        2) 배열 초기화
+ *            2.1 명시적 초기화
+ *                 int[] arr={1,2,3,4,5}
+ *         ***2.2 파일 읽기
+ *         ***2.3 데이터 베이스 읽기
+ *         ***2.4 크롤링
+ *            2.5 임의 추출
+ *            2.6 입력값
+ *                 
+ *        3) 배열 출력
+ *            1.1 일반 for : 인덱스 번호
+ *            1.2 for-each : 출력만 하는 경우
+ *        4) 정수 배열 / 문자열 배열    
+ *      ------------------------------------ 배열 사용 방법   
+ *         
+ *      2. 배열의 특징
+ *        1) 변수 : 한개만 저장하는 메모리 공간
+ *           -------------------------- 
+ *           | 변수 여러개를 묶어서 한번에 저장공간 설정 => 배열
+ *           | 데이터 관리를 쉽게 만들어 준다
+ *           | 익숙하지 않아서 어려워 보인다             ex) int[] arr=new int[12000] => 변수로 12000개를 만드는 것보다 좋다 
+ *           
+ *          *문법 => 데이터형 / 변수 / 연산자 / 제어문
+ *           --- 묶는 것
+ *               ----- 라이브러리를 연결 => 조립
+ *                                     ---- ***
+ *          ex) 로그인  : if ~ else
+ *              회원가입 : if ~else
+ *              목록출력 : for
+ *              예약    : if / for
+ *              
+ *          => 메모리를 연속적으로 생성 => 데이터형(메모리 크기) 동일
+ *             => 변수명을 자동으로 부여 (인덱스 번호)
+ *             
+ *                                            
+ *      3. 배열의 단점
+ *         1) 한번 선언하면 변경이 불가능
+ *            int[] arr=new int[5];
+ *            arr=new int[10]; <- 주소가 바뀌기 때문에 기존 데이터 사라짐            
+ */
+public class 배열정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int[] arr=new int[5];
+//		System.out.println(arr);
+//		arr=new int[10];
+//		System.out.println(arr);
+		
+		// clone() , 대입
+//		int[] arr=new int[5];
+//		System.out.println(arr);
+//		int[] arr2=arr;
+//		System.out.println(arr2);
+//		
+//		arr2[0]=100;
+//		System.out.println(arr[0]);
+//		
+//		int[] arr3=arr.clone();
+//		arr3[0]=1000;
+//		System.out.println(arr[0]);
+		
+		//크기 늘리기
+//		int[] arr={1,2,3,4,5};
+//		int[] arr2=new int[10];
+//		for(int i=0; i<arr.length; i++) {
+//			arr2[i]=arr[i];
+//		}
+		
+		
+		
+		
+				
+
+	}
+
+}
