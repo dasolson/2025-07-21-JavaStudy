cd4488db7d2fb5c4a7434653c5bd0ce7c42d338d
sist <sist@sist24>
2025-08-07 17:50:16 +0900

    메소드 2

---
+++ Chapter06_객체지향프로그램(메소드)/src/member.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+public class member {
+	private String id;
+	private String PWD;
+	private String name;
+	private String sex;
+	private String age;
+}
